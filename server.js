9533fe494cc172b6c296f5085c2c701fc8d08804
Felipe Souza <87735540+felipe-souza17@users.noreply.github.com>
2023-05-21 12:22:52 -0300

    Novas rotas

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const http = require('http')
 const port = 3000
 
+const rotas = {
+  '/': 'Curso de Node',
+  '/livros': 'Entrei na pag de livros',
+  '/autores': 'Listagem de autores',
+  '/editora': 'Editora do livro.'
+}
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Curso de Node.')
+  res.end(rotas[req.url])
 })
 
 server.listen(port, () =>

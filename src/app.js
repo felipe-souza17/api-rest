6599e8d45b87bbbba0c63dd7a2f9c69c7119618e
Felipe Souza <87735540+felipe-souza17@users.noreply.github.com>
2023-05-21 21:25:38 -0300

    Listagem de livro específico

--- src/app.js
+++ src/app.js
@@ -23,6 +23,12 @@ app.get('/livros', (req, res) => {
   res.status(200).json(livros)
 })
 
+app.get('/livros/:id', (req, res) => {
+  const index = buscaLivro(req.params.id)
+
+  res.json(livros[index])
+})
+
 app.post('/livros', (req, res) => {
   livros.push(req.body)
   res.status(201).json('Livro foi cadastrado com sucesso!!')

b1e904a6b06103aabf743545a31c795510df26ca
Felipe Souza <87735540+felipe-souza17@users.noreply.github.com>
2023-05-31 20:34:28 -0300

    Removido rota do app.js de cadastro

--- src/app.js
+++ src/app.js
@@ -22,11 +22,6 @@ app.get('/livros/:id', (req, res) => {
   res.json(livros[index])
 })
 
-app.post('/livros', (req, res) => {
-  livros.push(req.body)
-  res.status(201).json('Livro foi cadastrado com sucesso!!')
-})
-
 app.put('/livros/:id', (req, res) => {
   const index = buscaLivro(req.params.id)
 

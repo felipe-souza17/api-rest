f3d2a68d8669954fe06d50e0512d02a007812279
Felipe Souza <87735540+felipe-souza17@users.noreply.github.com>
2023-05-31 19:56:56 -0300

    Adicionado rota usando o roteamento do express

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import livros from './livrosRoutes.js'
+
+/**
+ * Recebe nosso app e gerencia todas as nossas rotas que estão dentro de /routes e depois exporta para ser usado em app.js
+ */
+const routes = app => {
+  app.route('/').get((req, res) => {
+    res.status(200).send({ titulo: 'Curso de Node com Mongoose.' })
+  })
+
+  app.use(express.json(), livros)
+}
+
+export default routes
---
+++ src/routes/livrosRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import LivroController from '../controllers/livrosController.js'
+
+/**
+ * Usa o roteamento do express para que busque através do controlador quando acessar a rota /livros através do método GET e por fim exporta para usar no nosso index.js
+ */
+const router = express.Router()
+
+router.get('/livros', LivroController.listarLivros)
+
+export default router

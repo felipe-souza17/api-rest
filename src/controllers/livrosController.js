e371b126610b39286fca365a4cf7dbe77018c386
Felipe Souza <87735540+felipe-souza17@users.noreply.github.com>
2023-05-31 20:33:54 -0300

    Implementado método de cadastro de um livro

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -9,6 +9,19 @@ class LivroController {
     const livro = await livros.find({})
     res.status(200).json(livro)
   }
+
+  static cadastrarLivro = async (req, res) => {
+    let livro = new livros(req.body)
+
+    try {
+      await livro.save()
+
+      res.status(201).send(livro.toJSON())
+    } catch (err) {
+      console.error(err)
+      res.status(500).send({ message: 'Erro ao cadastrar livro.' })
+    }
+  }
 }
 
 export default LivroController

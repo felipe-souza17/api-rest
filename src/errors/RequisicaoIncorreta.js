f55ab2ec3a53ecad491e84d14962ae01e9548ce2
Felipe Souza <87735540+felipe-souza17@users.noreply.github.com>
2023-07-23 11:20:43 -0300

    Feat (Erro): Refatoramento do erro de validação

--- src/errors/RequisicaoIncorreta.js
+++ src/errors/RequisicaoIncorreta.js
@@ -1,8 +1,8 @@
 import ErroBase from "./ErroBase.js";
 
 class RequisicaoIncorreta extends ErroBase {
-  constructor() {
-    super("Um ou mais dados fornecidor estão incorretos", 400);
+  constructor(mensagem = "Um ou mais dados fornecidor estão incorretos") {
+    super(mensagem, 400);
   }
 }
 
--- src/middlewares/manipularDeErros.js
+++ src/middlewares/manipularDeErros.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import ErroBase from "../errors/ErroBase.js";
 import RequisicaoIncorreta from "../errors/RequisicaoIncorreta.js";
+import ErroValidacao from "../errors/ErroValidacao.js";
 
 // eslint-disable-next-line no-unused-vars
 export default function manipularDeErros(erro, req, res, next) {
@@ -8,12 +9,7 @@ export default function manipularDeErros(erro, req, res, next) {
   if (erro instanceof mongoose.Error.CastError) {
     new RequisicaoIncorreta().enviarResposta(res);
   } else if (erro instanceof mongoose.Error.ValidationError) {
-    const mensagensErro = Object.values(erro.errors)
-      .map(erro => erro.message)
-      .join("; ");
-    res.status(400).send({
-      message: `Os seguintes erros foram encontrados: ${mensagensErro}`
-    });
+    new ErroValidacao(erro).enviarResposta(res);
   } else {
     new ErroBase().enviarResposta(res);
   }

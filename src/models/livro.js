d99432d87e0b974940210e24110e7a0cec216749
Felipe Souza <87735540+felipe-souza17@users.noreply.github.com>
2023-07-23 12:30:49 -0300

    Feat (Validação): Implementado validações na editora e página de um livro

--- src/models/livro.js
+++ src/models/livro.js
@@ -8,8 +8,25 @@ const livroSchema = new mongoose.Schema({
     ref: "autores",
     required: [true, "O(a) autor(a) é obrigatório"]
   },
-  editora: { type: String, required: [true, "A editora é obrigatória"] },
-  numeroPaginas: { type: Number }
+  editora: {
+    type: String,
+    required: [true, "A editora é obrigatória"],
+    enum: {
+      values: ["Casa do código", "Alura"],
+      message: "A editora {VALUE} não é um valor permitido."
+    }
+  },
+  numeroPaginas: {
+    type: Number,
+    min: [
+      10,
+      "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"
+    ],
+    max: [
+      5000,
+      "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"
+    ]
+  }
 });
 
 const livros = mongoose.model("livros", livroSchema);

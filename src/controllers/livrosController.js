2506e543885c3f9b9eb77618fb9d9c244c7eefa8
felipe-souza17 <felipesantos.rsouza@gmail.com>
2023-07-25 16:44:11 -0300

    Feat (Livros): Ordenando os resultados com .sort()

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -9,14 +9,17 @@ import { autores, livros } from "../models/index.js";
 class LivroController {
   static listarLivros = async (req, res, next) => {
     try {
-      let { limite = 5, pagina = 1 } = req.query;
+      let { limite = 5, pagina = 1, ordenacao = "_id:-1" } = req.query;
+      let [campoOrdenacao, ordem] = ordenacao.split(":");
 
       limite = parseInt(limite);
       pagina = parseInt(pagina);
+      ordem = parseInt(ordem);
 
       if (limite > 0 && pagina > 0) {
         const livro = await livros
           .find({})
+          .sort({ [campoOrdenacao]: ordem })
           .skip((pagina - 1) * limite)
           .limit(limite)
           .populate("autor", "nome");
